cb92c75bb54bbb37b9312d33ac5d297441ac5587
Thapson Nyirenda <thapsonknyirenda@gmail.com>
2024-01-04 09:55:19 +0200

    i have added some files

--- index.php
+++ index.php
@@ -4,13 +4,19 @@
         public $name;
         public $email;  
 
+        public function __construct($name, $email) { 
+            $this->name = $name;
+            $this->email = $email; 
+        }
+
         public function login(){
-            echo "the user is logged in";
+            echo $this->name. " is logged in";
         }
     }
 
-    $userOne=new User();
+    $userOne=new User('Thapson','thap@gmail.com');
 
+    // $userOne->login();
     $userOne->login();
 
 ?>
