cb4f5e9d757c185e521ce894dd79cee1b5b991e8
Thapson Nyirenda <thapsonknyirenda@gmail.com>
2024-01-08 19:01:54 +0200

    i have added some files

--- index.php
+++ index.php
@@ -1,22 +1,62 @@
+<!-- 
+Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+The value of |x| is defined as:
+x if x >= 0.
+-x if x < 0.
+
+
+Example 1
+Input: nums = [1,2,2,1], k = 1
+Output: 4
+Explanation: The pairs with an absolute difference of 1 are:
+- [1,2,2,1]
+- [1,2,2,1]
+- [1,2,2,1]
+- [1,2,2,1]
+
+
+Example 2:
+Input: nums = [1,3], k = 3
+Output: 0
+Explanation: There are no pairs with an absolute difference of 3.
+
+
+Example 3:
+Input: nums = [3,2,1,5,4], k = 2
+Output: 3
+Explanation: The pairs with an absolute difference of 2 are:
+- [3,2,1,5,4]
+- [3,2,1,5,4]
+- [3,2,1,5,4]
+
+
+Constraints:
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+1 <= k <= 99 -->
+
+
 <?php
 
-    class User{
-        public $name;
-        public $email;  
+    function countAbsolutePairs($num, $k){
+         
+        $count =0;
+        $length= count($num);
 
-        public function __construct($name, $email) { 
-            $this->name = $name;
-            $this->email = $email; 
+        for($i= 0; $i<$length; $i++){
+            for($j= $i + 1; $j< $length; $j++){
+                $absoluteValue= abs($num[$i]-$num[$j]);
+
+                if($k== $absoluteValue){
+                    $count++;
+                }
+            }
         }
 
-        public function login(){
-            echo $this->name. " is logged in";
-        }
+        return $count;
     }
 
-    $userOne=new User('Thapson','thap@gmail.com');
-
-    // $userOne->login();
-    $userOne->login();
+    echo countAbsolutePairs([1,2,2,1], 1);
 
 ?>
+
